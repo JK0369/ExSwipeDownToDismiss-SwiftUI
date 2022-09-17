c8f0f41ff82607077d83421e17c33dceec5cffa3
jake <palatable7@naver.com>
2022-09-17 13:38:19 +0900

    Initial Commit

---
+++ ExMoveView/ExMoveViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExMoveViewApp.swift
+//  ExMoveView
+//
+//  Created by 김종권 on 2022/09/17.
+//
+
+import SwiftUI
+
+@main
+struct ExMoveViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
